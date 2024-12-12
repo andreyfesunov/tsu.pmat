f4cb618f86f03ded79cd57d143c1e48900bc1176
andreyfesunov <fsnv04@gmail.com>
2024-12-12 23:48:05 +0300

    bug: lab2, make work with tty

--- lab2/greeter.py
+++ lab2/greeter.py
@@ -4,26 +4,38 @@ import re
 from sys import stderr, stdin
 
 
-def validate_name(name: str):
+def validate_name(name: str) -> None:
     if not bool(re.match(r"^[A-Za-z]+$", name)):
         raise Exception("Wrong name pattern. Name must consist of letters")
     if not name[0].isupper():
         raise Exception("Wrong name pattern. Name must begin with capital letter")
 
 
-def greet_names(names: list[str]):
-    for name in names:
-        try:
-            validate_name(name)
-            print(f"Nice to see you {name}!")
-        except Exception as e:
-            print(f"Error: {str(e)}", file=stderr)
-
-
-def main():
+def greet_name(name: str) -> None:
     try:
-        names = [line.strip() for line in stdin]
-        greet_names(names)
+        validate_name(name)
+        print(f"Nice to see you {name}!")
+    except Exception as e:
+        print(f"Error: {str(e)}", file=stderr)
+
+
+def tty_greeter() -> None:
+    while True:
+        print("Hey, what's your name?")
+        greet_name(stdin.readline().strip())
+
+
+def pipeline_greeter() -> None:
+    for line in stdin:
+        greet_name(line.strip())
+
+
+def main() -> None:
+    try:
+        if stdin.isatty():
+            tty_greeter()
+        else:
+            pipeline_greeter()
     except KeyboardInterrupt:
         print("\nGoodbye!")
 
